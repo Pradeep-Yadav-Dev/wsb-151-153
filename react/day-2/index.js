6e3a3616b5d4de5807b2d21ec8fea49a499e9bb4
Pradeep Kumar Yadav <pradeepkumar.mail029@gmail.com>
2025-04-11 10:07:09 +0530

    node

---
+++ react/day-2/index.js
@@ -0,0 +1,52 @@
+// let a=[11,22,33,44,55,66]
+
+// // for(let i=1 ; i<=a.length ; i++){
+// //     console.log(i)
+// // }
+
+// let newData=a.map((v,i)=>{
+//     // console.log(v,i)
+// })
+
+// a.filter((v,i)=>{
+//     if(v%2==0){
+//         // console.log(v)
+//     }
+// })
+
+// let x=[11,22,33,44]
+// let z=[55,66,77,88]
+
+// console.log([...x,...z])
+
+// console.log([...x,"pradeep"])
+
+
+// let ws={
+//     name:"p",
+//     class:10
+// }
+
+// console.log({...ws} )
+
+
+//////////////////////////////////////////////////
+
+// import {Data} from "./ws.js"
+// import {udaipur} from "./ws.js"
+// console.log(Data)
+
+// console.log(udaipur)
+
+///////////////////////////////////////////
+
+// import sum from "./ws.js"
+
+// console.log(sum)
+
+
+///////////////////// for backend
+
+let pk=require("./ws")
+
+console.log(pk)
---
+++ react/day-2/ws.js
@@ -0,0 +1,23 @@
+// export let Data="welcome to ws"
+
+//  export let udaipur="welcome to udaipur"
+
+//////////////////////////////////////////////
+
+//  let sum=()=>{
+//     console.log("hello class")
+// }
+
+// export default sum
+
+
+// let ps=()=>{
+//     console.log("hello class")
+// }
+
+// export default ps
+
+////////////////////////////////////
+
+let pk="hello wsb"
+module.exports=pk
